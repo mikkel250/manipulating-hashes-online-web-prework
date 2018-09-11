d76f7904036d08a4bfd394a21229c0839f73bf7c
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 19:29:26 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   
   contacts.each do |person, data|
     data.each do |attribute, value| if person == "Freddy Mercury"
-      attribute.delete(value) if attribute[value] == "strawberry"
+      attribute.delete(value) if attribute[value] == "strawberry" #this is an array!
       #end
     end
   #remember to return your newly altered contacts hash!
