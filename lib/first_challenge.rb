e598b405b432e8c23f371e056f2e101b85e7feed
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 22:02:39 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ def first_challenge
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
-      ind = value()
+      ind = value.index("strawberry")
        value.slice!(flavor, 1) if flavor == "strawberry"
         
       end
