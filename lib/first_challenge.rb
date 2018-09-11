b25d6cb6cf5edcfd46cb4ad5ee6e062ecaa40b52
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 21:52:37 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,7 @@ def first_challenge
     if attribute == :favorite_icecream_flavors
       value.each do |flavor|
        puts value[flavor]
-       puts value[flavor.index]
+       puts value[flavor].index
        value[flavor.index].slice!(flavor.index, 1) if value[flavor] == "strawberry"
         
       end
